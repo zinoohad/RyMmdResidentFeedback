b4df165ff36937c70094c846cba62257ba7f0c0d
zinoohad <zinoohad@gmail.com>
2026-06-26 11:38:05 +0000

    Config update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Update after deploying the Apps Script backend.
 window.RESIDENT_FEEDBACK_CONFIG = {
-  API_BASE_URL: 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE',
-  PUBLIC_API_KEY: 'PASTE_PUBLIC_API_KEY_FROM_SETTINGS_SHEET_HERE',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyV7IQWiUzyZSX64zQaCpcsGZGmwJqEXD-rsbqU_9IUBYD_Zm3iK8wuY5kTj-ZYlitj6A/exec',
+  PUBLIC_API_KEY: 'f1e3d9cb-9e4a-4299-9c55-7be53c7f8c7a',
   APP_TITLE: 'התייחסות דיירים לתכנון מיקום ממ״ד',
   MAX_FILE_SIZE_BYTES: 10 * 1024 * 1024,
   MAX_FILES: 5
